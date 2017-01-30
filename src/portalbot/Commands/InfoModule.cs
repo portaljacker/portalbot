58e41f3492a6904d8e37660efb105c6a4f854396
Jordan Victor <portaljacker@gmail.com>
2017-01-30 02:57:25 -0500

    Add Command handling, !say command, and clean code
    * !ping command moved into InfoModule
    * Comments from tutorial mostly removed

---
+++ src/portalbot/Commands/InfoModule.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace portalbot.Commands
+{
+    public class InfoModule : ModuleBase
+    {
+        [Command("ping"), Summary("pong!.")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong!");
+        }
+
+        [Command("say"), Summary("Echos a message.")]
+        public async Task Say([Remainder, Summary("The text to echo")] string echo)
+        {
+            var userInfo = Context.User;
+            await Context.Channel.SendMessageAsync($"{userInfo.Username} wants me to say, \"{echo}\" frankly I think that's a ridiculous expectation on their part, now don't you?");
+        }
+    }
+}
--- src/portalbot/Program.cs
+++ src/portalbot/Program.cs
@@ -1,39 +1,63 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace portalbot
 {
     class Program
     {
-        // Convert our sync-main to an async main method
-        static void Main(string[] args) => new Program().Run().GetAwaiter().GetResult();
+        private CommandService _commands;
+        private DiscordSocketClient _client;
+        private DependencyMap _map;
 
-        // Create a DiscordClient with WebSocket support
-        private DiscordSocketClient client;
+        static void Main(string[] args) => new Program().Run().GetAwaiter().GetResult();
 
         public async Task Run()
         {
-            client = new DiscordSocketClient();
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
 
-            // Place the token of your bot account here
             string token = "aaabbbcc";
 
-            // Hook into the MessageReceived event on DiscordSocketClient
-            client.MessageReceived += async (message) =>
-            {   // Check to see if the Message Content is "!ping"
-                if (message.Content == "!ping")
-                    // Send 'pong' back to the channel the message was sent in
-                    await message.Channel.SendMessageAsync("pong");
-            };
+            _map = new DependencyMap();
 
-            // Configure the client to use a Bot token, and use our token
-            await client.LoginAsync(TokenType.Bot, token);
-            // Connect the client to Discord's gateway
-            await client.ConnectAsync();
+            await InstallCommands();
+
+            /*_client.MessageReceived += async (message) =>
+            {
+                if (message.Content == "!ping")
+                    await message.Channel.SendMessageAsync("pong");
+            };//*/
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.ConnectAsync();
 
             // Block this task until the program is exited.
             await Task.Delay(-1);
+        }
+
+        public async Task InstallCommands()
+        {
+            _client.MessageReceived += HandleCommand;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (!message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) return;
+
+            var context = new CommandContext(_client, message);
+
+            var result = await _commands.ExecuteAsync(context, argPos, _map);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
         }
     }
 }
