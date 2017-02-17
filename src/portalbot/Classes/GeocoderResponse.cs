0be7b2d3a185362161bd309c880c1902ff00ab20
Jordan Victor <portaljacker@gmail.com>
2017-02-16 22:27:30 -0500

    GH-3 Remove unneded JsonProperty attributes

--- src/portalbot/Classes/GeocoderResponse.cs
+++ src/portalbot/Classes/GeocoderResponse.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-
-namespace portalbot.Classes
+﻿namespace portalbot.Classes
 {
     public class GeocoderResponse
     {
@@ -13,19 +11,14 @@ namespace portalbot.Classes
     {
         public string[] Types { get; set; }
 
-        [JsonProperty(PropertyName = "formatted_address")]
         public string FormattedAddress { get; set; }
 
-        [JsonProperty(PropertyName = "address_components")]
         public AddressComponent[] AddressComponents { get; set; }
 
-        [JsonProperty(PropertyName = "partial_match")]
         public bool PartialMatch { get; set; }
 
-        [JsonProperty(PropertyName = "place_id")]
         public string PlaceId { get; set; }
 
-        [JsonProperty(PropertyName = "postcode_localities")]
         public string[] PostCodeLocalities { get; set; }
 
         public Geometry Geometry { get; set; }
@@ -33,14 +26,11 @@ namespace portalbot.Classes
 
     public class AddressComponent
     {
-        [JsonProperty(PropertyName = "short_name")]
         public string ShortName { get; set; }
 
-        [JsonProperty(PropertyName = "long_name")]
         public string LongName { get; set; }
 
-        [JsonProperty(PropertyName = "postcode_localities")]
-        public string[] PostalCodeLocalities { get; set; }
+        public string[] PostCodeLocalities { get; set; }
 
         public string[] Types { get; set; }
     }
@@ -49,7 +39,6 @@ namespace portalbot.Classes
     {
         public LatLng Location { get; set; }
 
-        [JsonProperty(PropertyName = "location_type")]
         public string LocationType { get; set; }
 
         public LatLngBounds Viewport { get; set; }
